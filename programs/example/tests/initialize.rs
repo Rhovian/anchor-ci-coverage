91b3b480f5fe38058e6aeee39fdcef76f3e519ed
rhovian <atomicjab@proton.me>
2023-11-23 14:45:23 -0600

    example key & import in test

--- programs/example/tests/initialize.rs
+++ programs/example/tests/initialize.rs
@@ -2,12 +2,15 @@
 
 use anchor_client::{Client, Cluster, Program};
 use anchor_lang::AccountDeserialize;
-#[cfg(feature = "test-coverage")]
-use grcov::LcovFile;
-use solana_program_test::{processor, tokio, ProgramTest};
 
 #[test]
-fn test_initialize() {
+fn test_initialize() -> Result<(), Box<dyn Error>>{
+    let key: Rc<Keypair> = Rc::new(
+        read_keypair_file("../../example.json").unwrap(),
+    );
+
     let client: Client = Client::new(Cluster::Localnet, Rc::clone(&key) as Rc<dyn Signer>);
     let program: Program = client.program(example::ID);
+
+    Ok(())
 }
