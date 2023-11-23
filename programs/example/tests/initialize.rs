079e27fadc5b5956a8ac3ca2a41e21f0f74200db
rhovian <atomicjab@proton.me>
2023-11-23 13:01:39 -0600

    nightly

--- programs/example/tests/initialize.rs
+++ programs/example/tests/initialize.rs
@@ -1,13 +1,13 @@
 #![cfg(feature = "test-sbf")]
 
+use anchor_client::{Client, Cluster, Program};
+use anchor_lang::AccountDeserialize;
 #[cfg(feature = "test-coverage")]
 use grcov::LcovFile;
-use anchor_lang::AccountDeserialize;
-use solana_program_test::{ProgramTest, processor, tokio};
-use anchor_client::{Client, Cluster, Program};
+use solana_program_test::{processor, tokio, ProgramTest};
 
 #[test]
 fn test_initialize() {
-  let client: Client = Client::new(Cluster::Localnet, Rc::clone(&key) as Rc<dyn Signer>);
-  let program: Program = client.program(example::ID);
+    let client: Client = Client::new(Cluster::Localnet, Rc::clone(&key) as Rc<dyn Signer>);
+    let program: Program = client.program(example::ID);
 }
