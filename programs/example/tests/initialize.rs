1a59b8f5b561447f30aa466a83cbcc59a050b814
rhovian <atomicjab@proton.me>
2023-11-24 01:28:23 -0600

    reset

--- programs/example/tests/initialize.rs
+++ programs/example/tests/initialize.rs
@@ -4,10 +4,8 @@ use anchor_client::{Client, Cluster, Program};
 use anchor_lang::AccountDeserialize;
 
 #[test]
-fn test_initialize() -> Result<(), Box<dyn Error>>{
-    let key: Rc<Keypair> = Rc::new(
-        read_keypair_file("../../example.json").unwrap(),
-    );
+fn test_initialize() -> Result<(), Box<dyn Error>> {
+    let key: Rc<Keypair> = Rc::new(read_keypair_file("../../example.json").unwrap());
 
     let client: Client = Client::new(Cluster::Localnet, Rc::clone(&key) as Rc<dyn Signer>);
     let program: Program = client.program(example::ID);
