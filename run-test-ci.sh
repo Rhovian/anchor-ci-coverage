6f01ac3132d3b22b0c54fc3bcd0e8e3cf8e4bf41
rhovian <atomicjab@proton.me>
2023-12-18 22:25:45 -0600

    attempt ci

--- run-test-ci.sh
+++ run-test-ci.sh
@@ -9,6 +9,10 @@ main() {
   program_so="./target/deploy/example.so"
   keypath="$home_dir/.config/solana/id.json"
 
+  echo "Key balance: $(solana balance $solana_key)"
+  echo "Config:"
+  solana config get
+
   solana program deploy \
     --program-id $program_id \
     $program_so \
