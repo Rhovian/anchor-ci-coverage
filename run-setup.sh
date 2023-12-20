5446baedb57fd0d367f25ef6358dbdd9b10a65f5
rhovian <atomicjab@proton.me>
2023-12-20 14:06:46 -0600

    update ci

---
+++ run-setup.sh
@@ -0,0 +1,10 @@
+# clean anchor workspace, build anchor, sync keys
+npm run anchor:keys:sync
+
+# Get the new PROGRAM_ID
+NEW_ID=$(npm run anchor:keys:list | grep leveller | cut -d: -f2 | tr -d '[:space:]')
+
+echo "New PROGRAM_ID: $NEW_ID"
+
+# update the PROGRAM_ID in the SDK's address.ts file
+sed -i '' "s/\(export const PROGRAM_ID = new PublicKey(\)\".*\"/\1\"$NEW_ID\"/" ../packages/anchor/sdk/address.ts
