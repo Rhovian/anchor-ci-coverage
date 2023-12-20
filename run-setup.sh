fc051f5f3da055d4a10dc7eef77fc16485550e2f
rhovian <atomicjab@proton.me>
2023-12-20 14:10:57 -0600

    update ci

--- run-setup.sh
+++ run-setup.sh
@@ -7,4 +7,4 @@ NEW_ID=$(npm run anchor:keys:list | grep leveller | cut -d: -f2 | tr -d '[:space
 echo "New PROGRAM_ID: $NEW_ID"
 
 # update the PROGRAM_ID in the SDK's address.ts file
-sed -i '' "s/\(export const PROGRAM_ID = new PublicKey(\)\".*\"/\1\"$NEW_ID\"/" ../packages/anchor/sdk/address.ts
+sed -i "s/\(export const PROGRAM_ID = new PublicKey(\)\".*\"/\1\"$NEW_ID\"/" ./packages/anchor/sdk/address.ts
